9814f99aaee2aadc69e8a7db7fa9a2669eef26f1
NishthaN <nishthanayar98@gmail.com>
2020-04-26 21:17:25 +0530

    npm script 1

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+   <!-- jQuery first, then Popper.js, then Bootstrap JS. -->
+
+        $(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 2000 } );
+            $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');
+                }
+            });
+
+        });
+
+    $(document).ready(function(){
+	$('#ReserveButton').click(function(){
+  		$('#reservationModal').modal('show')
+	});
+});
+$(document).ready(function(){
+	$('#LoginButton').click(function(){
+  		$('#loginModal').modal('show')
+	});
+});
